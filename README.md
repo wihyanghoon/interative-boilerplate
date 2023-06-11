a0aae441927375045ce8178825790d87e5c7622c
wihyanghoon <gidgns1995@gmail.com>
2023-06-11 11:09:41 +0900

    ch1. loop javascript

---
+++ README.md
@@ -0,0 +1,9 @@
+인터렉티브 자바스크립트 보일러플레이트 모음집
+```
+interactive
+├─ REAME.md 
+└─ loop-slide
+   ├─ index.css
+   ├─ index.html
+   └─ index.js
+```
---
+++ loop-slide/index.js
@@ -0,0 +1,29 @@
+// Dom 변수선언 및 할당
+const slides = document.querySelector(".slides");
+const slide = document.querySelectorAll(".slides li");
+const currentIdx = 0; // 인덱스값
+const slideCount = slide.length;
+const slideWidth = 200;
+const SlideMargin = 30;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+
+// 앞뒤로 새로운 리스트 생성하는 함수
+const makeClone = () => {
+  //뒤에 붙이기
+  for (let i = 0; i < slideCount; i++) {
+    let cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.appendChild(cloneSlide);
+  }
+
+  //앞에 붙이기
+  for (let i = slideCount - 1; i >= 0; i--) {
+    let cloneSlide = slide[i].cloneNode(true);
+    cloneSlide.classList.add("clone");
+    slides.prepend(cloneSlide);
+  }
+};
+
+makeClone();
